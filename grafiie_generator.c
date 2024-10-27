3fb772eda84b1c1dec7063205c1c02f18708d4b2
Abi <abik4002@gmail.com>
2024-10-27 09:20:34 +0530

    Number ASCII Art Generator!!
    ASCII Art Number Generator in C
    
    This project generates ASCII art representations of numbers from 1 to 9 using the C programming language. In the future, more numbers and possibly text characters will be added to expand the functionality. The project demonstrates basic C concepts and helps in understanding loops and conditional structures in a fun, visual way

---
+++ grafiie_generator.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a no between 1 to 9 to turn it into a art:\n");
+    scanf("%d",&a);
+    if (a == 1)
+    {
+        printf("   1  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf("  11  \n");
+        printf(" 1111 \n");
+    }
+    else if (a == 2)
+    {
+        printf(" 2222  \n");
+        printf("2    2 \n");
+        printf("     2 \n");
+        printf("   222 \n");
+        printf("  2    \n");
+        printf(" 2     \n");
+        printf("22222  \n");
+    }
+    else if (a == 3)
+    {
+        printf(" 3333  \n");
+        printf("3    3 \n");
+        printf("     3 \n");
+        printf("  3333 \n");
+        printf("     3 \n");
+        printf("3    3 \n");
+        printf(" 3333  \n");
+    }
+    else if (a == 4)
+    {
+        printf("   44   \n");
+        printf("  444  \n");
+        printf(" 4  4  \n");
+        printf("4   4  \n");
+        printf("4444444 \n");
+        printf("    4  \n");
+        printf("    4  \n");
+    }
+    else if (a == 5)
+    {
+        printf(" 5555  \n");
+        printf("5      \n");
+        printf("5      \n");
+        printf(" 5555  \n");
+        printf("     5  \n");
+        printf("5    5  \n");
+        printf(" 5555  \n");
+    }
+    else if (a == 6)
+    {
+        printf("  6666  \n");
+        printf(" 6      \n");
+        printf("6       \n");
+        printf("6   666 \n");
+        printf("6    66 \n");
+        printf(" 6   66 \n");
+        printf("  6666  \n");
+    }
+    else if (a == 7)
+    {
+        printf("7777777\n");
+        printf("     7 \n");
+        printf("    7  \n");
+        printf("   7   \n");
+        printf("  7    \n");
+        printf(" 7     \n");
+        printf("7      \n");
+    }
+    else if (a == 8)
+    {
+        printf("  8888  \n");
+        printf(" 8    8 \n");
+        printf("8      8\n");
+        printf("  8888  \n");
+        printf("8      8 \n");
+        printf(" 8    8 \n");
+        printf("  8888  \n");
+    }
+    else if (a == 9)
+    {
+        printf("  9999  \n");
+        printf(" 9    9 \n");
+        printf("9      9\n");
+        printf("  9999  \n");
+        printf("      9 \n");
+        printf(" 9    9 \n");
+        printf("  9999  \n");
+    }
+    else if (a > 9)
+    {
+
+    printf("  _____              _     \n");
+    printf(" / ____|            | |    \n");
+    printf("| (___   ___   ___  | |__  \n");
+    printf(" \\___ \\ / _ \\ / _ \\ | '_ \\ \n");
+    printf(" ____) |  __/|  __/ | | | |\n");
+    printf("|_____/ \\___| \\___| |_| |_|\n");
+    printf("                               \n");
+    printf("Enter a no between 1 to 9");
+    }
+
+    return 0;
+}
